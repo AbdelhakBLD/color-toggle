c16ed4c7eb0d02bcb7ab12870b9c4ec5e1d95d8a
AbdelhakBLD <abdelhak.belaid@outlook.com>
2020-11-16 15:37:35 +0100

    update script toggle color

--- assests/script/index.js
+++ assests/script/index.js
@@ -1,14 +1,18 @@
 let colorP = document.querySelector("p#colorP");
 let btn = document.querySelector("button#btn");
 
-let checkColor = false;
-const changeColor = () => {
-  if (!checkColor) {
-    colorP.className = "blue";
-    checkColor = true;
-  } else {
-    colorP.className = "red";
-    checkColor = false;
-  }
-};
-btn.addEventListener("click", changeColor);
+btn.addEventListener("click", () => {
+  colorP.classList.toggle('red');
+})
+
+// let checkColor = false;
+// const changeColor = () => {
+//   if (!checkColor) {
+//     colorP.className = "blue";
+//     checkColor = true;
+//   } else {
+//     colorP.className = "red";
+//     checkColor = false;
+//   }
+// };
+// btn.addEventListener("click", changeColor);
